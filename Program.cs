76f07bfdbab688ed67201c3bbef176d1a8b59504
Diana <kosyanenkodiana@yandex.ru>
2022-12-22 18:34:24 +0300

    rep

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿/*void Degree(int a, int b){
+    int step = 1;
+    for (int i = 1; i <= b; i++)
+    {
+         step = step * a;
+    }
+    Console.WriteLine(step);
+}
+
+Console.WriteLine("Enter number a");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter number b");
+int b = Convert.ToInt32(Console.ReadLine());
+Degree(a, b);*/
+
+/*void Counter(int num){
+
+    int sum = 1;
+    for (int i = 1; i < num; i++)
+    {
+        sum += (num % 10);
+        num /= 10;
+    }
+
+   Console.WriteLine(sum);
+}
+Console.WriteLine("Enter your number");
+int num = Convert.ToInt32(Console.ReadLine());
+Counter(num);*/
+
+/*int[] Array(int num)
+{
+   
+    int [] arr = new int [num];
+    for (int i = 0; i < num; i++)
+    {
+      arr[i] = num;
+    }
+    return arr;
+}
+void ShowArray (int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+}
+
+Console.WriteLine("Write");
+int lengthMass = Convert.ToInt32(Console.ReadLine());
+int num = Convert.ToInt32(Console.ReadLine());
+ShowArray(Array(lengthMass));*/
